f088d97955aa534d0c957938384e10072087c69a
Alvin Nguyen <anh.ns.dev@gmail.com>
2026-05-06 15:13:31 +0700

    chore: add docs navigation

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,26 +1,26 @@
 export const navigation = [
   { label: 'Trang chủ', path: '/' },
   { label: 'Giới thiệu', path: '/gioi-thieu' },
-  // {
-  //   label: 'Công bố năng lực',
-  //   children: [
-  //     { label: 'Giấy đăng ký kinh doanh', path: '/thu-vien/giay-phep-kinh-doanh' },
-  //     {
-  //       label: 'Bản công bố thông tin năng lực hoạt động thí nghiệm',
-  //       path: '/thu-vien/cong-bo-nang-luc',
-  //     },
-  //     {
-  //       label: 'Giấy chứng nhận đủ điều kiện hoạt động thí nghiệm',
-  //       path: '/thu-vien/chung-nhan-du-dieu-kien',
-  //     },
-  //     {
-  //       label: 'Giấy chứng nhận hiệu chuẩn thiết bị',
-  //       path: '/thu-vien/hieu-chuan-thiet-bi',
-  //     },
-  //     { label: 'Danh mục thiết bị', path: '/thu-vien/danh-muc-thiet-bi' },
-  //     // { label: 'Danh sách cán bộ', path: '/thu-vien/danh-sach-can-bo' },
-  //   ],
-  // },
+  {
+    label: 'Công bố năng lực',
+    children: [
+      // { label: 'Giấy đăng ký kinh doanh', path: '/thu-vien/giay-phep-kinh-doanh' },
+      {
+        label: 'Bản công bố thông tin năng lực hoạt động thí nghiệm',
+        path: '/thu-vien/cong-bo-nang-luc',
+      },
+      // {
+      //   label: 'Giấy chứng nhận đủ điều kiện hoạt động thí nghiệm',
+      //   path: '/thu-vien/chung-nhan-du-dieu-kien',
+      // },
+      // {
+      //   label: 'Giấy chứng nhận hiệu chuẩn thiết bị',
+      //   path: '/thu-vien/hieu-chuan-thiet-bi',
+      // },
+      // { label: 'Danh mục thiết bị', path: '/thu-vien/danh-muc-thiet-bi' },
+      // { label: 'Danh sách cán bộ', path: '/thu-vien/danh-sach-can-bo' },
+    ],
+  },
   { label: 'Dịch vụ', path: '/dich-vu' },
   { label: 'Dự án', path: '/du-an' },
   { label: 'Tin tức', path: '/tin-tuc' },
